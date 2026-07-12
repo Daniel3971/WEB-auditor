e3ac15f38c2dcd92ad87852f016f49d6287eccbb
karim473 <karimmchaidar@gmail.com>
2026-07-12 16:40:48 +0300

    frontend modification

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Services } from './pages/services/services';
+import { Locations } from './pages/locations/locations';
+import { OfficialForms } from './pages/official-forms/official-forms';
+import { Careers } from './pages/careers/careers';
+import { Contact } from './pages/contact/contact';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'about',
+    component: About
+  },
+  {
+    path: 'services',
+    component: Services
+  },
+  {
+    path: 'locations',
+    component: Locations
+  },
+  {
+    path: 'official-forms',
+    component: OfficialForms
+  },
+  {
+    path: 'careers',
+    component: Careers
+  },
+  {
+    path: 'contact',
+    component: Contact
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,36 +1,18 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
-import { CompanyService } from './models/company-service';
-import { CompanyServiceApi } from './services/company-service-api';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    Navbar,
+    Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  services = signal<CompanyService[]>([]);
-  loading = signal(true);
-  errorMessage = signal('');
-
-  constructor(private companyServiceApi: CompanyServiceApi) {}
-
-  ngOnInit(): void {
-    this.loadServices();
-  }
-
-  loadServices(): void {
-    this.companyServiceApi.getAllServices().subscribe({
-      next: (services) => {
-        this.services.set(services);
-        this.loading.set(false);
-      },
-      error: (error) => {
-        console.error('Could not load services:', error);
-        this.errorMessage.set('Could not load services.');
-        this.loading.set(false);
-      }
-    });
-  }
+export class App {
 }
